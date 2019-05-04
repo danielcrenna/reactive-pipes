4f5cb94bb3c52b34038753c4d2d3b9dc0fa24124
Daniel Crenna <daniel.crenna@gmail.com>
2019-05-04 00:18:29 -0400

    nit: formatting

--- src/reactive.pipes.tests/EventPublisherTests.cs
+++ src/reactive.pipes.tests/EventPublisherTests.cs
@@ -1,92 +1,96 @@
+// Copyright (c) Daniel Crenna. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using reactive.pipes.tests.Fakes;
 using Xunit;
 
 namespace reactive.pipes.tests
 {
-    public class EventPublisherTests
-    {
-        [Fact]
-        public void Can_publish_events_by_type()
-        {
-            var handled = 0;
-            var hub = new Hub();
-            hub.Subscribe<InheritedEvent>(e => handled++);
-            object @event = new InheritedEvent {Id = 123, Value = "ABC"};
-            var sent = hub.Publish(@event);
+	public class EventPublisherTests
+	{
+		[Fact]
+		public void Can_handle_error_with_callback()
+		{
+			var errors = 0;
 
-            Assert.True(sent, "did not send event to a known subscription");
-            Assert.Equal(1, handled);
-        }
+			var hub = new Hub();
 
-        [Fact]
-        public void Can_handle_error_with_callback()
-        {
-            var errors = 0;
+			var sent = false;
 
-            var hub = new Hub();
+			// SubscribeWithDelegate:
+			{
+				hub.Subscribe<InheritedEvent>(e => { throw new Exception(); }, ex => { errors++; });
+				object @event = new InheritedEvent {Id = 123, Value = "ABC"};
+				sent = hub.Publish(@event);
+				Assert.False(sent, "publishing a failed event should bubble as false to the publish result");
+				Assert.Equal(1, errors);
+			}
 
-            bool sent = false;
+			// SubscribeWithDelegateAndTopic:
+			{
+				hub.Subscribe<StringEvent>(se => { throw new Exception(); }, @event => @event.Text == "bababooey!",
+					ex => { errors++; });
+				sent = hub.Publish(new StringEvent("not bababooey!"));
+				Assert.False(sent);
+				Assert.Equal(1, errors);
+				sent = hub.Publish(new StringEvent("bababooey!"));
+				Assert.False(sent);
+				Assert.Equal(2, errors);
+			}
 
-            // SubscribeWithDelegate:
-            {
-                hub.Subscribe<InheritedEvent>(e => { throw new Exception(); }, ex => { errors++; });
-                object @event = new InheritedEvent {Id = 123, Value = "ABC"};
-                sent = hub.Publish(@event);
-                Assert.False(sent, "publishing a failed event should bubble as false to the publish result");
-                Assert.Equal(1, errors);
-            }
+			// Subscribe (manifold):
+			{
+				var handler = new ManifoldHierarchicalEventHandler();
+				hub.Subscribe(handler, ex => { errors++; });
+				sent = hub.Publish(new ErrorEvent());
+				Assert.False(sent);
+				Assert.Equal(3, errors);
+			}
 
-            // SubscribeWithDelegateAndTopic:
-            {
-                hub.Subscribe<StringEvent>(se => { throw new Exception(); }, @event => @event.Text == "bababooey!", ex => { errors++; });
-                sent = hub.Publish(new StringEvent("not bababooey!"));
-                Assert.False(sent);
-                Assert.Equal(1, errors);
-                sent = hub.Publish(new StringEvent("bababooey!"));
-                Assert.False(sent);
-                Assert.Equal(2, errors);
-            }
+			// SubscribeWithInterface:
+			{
+				hub.Subscribe(new ErroringHandler());
+				sent = hub.Publish(new ErrorEvent());
+				Assert.False(sent);
+				Assert.Equal(4, errors);
+			}
+		}
 
-            // Subscribe (manifold):
-            {
-                ManifoldHierarchicalEventHandler handler = new ManifoldHierarchicalEventHandler();
-                hub.Subscribe(handler, ex => { errors++; });
-                sent = hub.Publish(new ErrorEvent());
-                Assert.False(sent);
-                Assert.Equal(3, errors);
-            }
+		[Fact]
+		public void Can_publish_events_by_type()
+		{
+			var handled = 0;
+			var hub = new Hub();
+			hub.Subscribe<InheritedEvent>(e => handled++);
+			object @event = new InheritedEvent {Id = 123, Value = "ABC"};
+			var sent = hub.Publish(@event);
 
-            // SubscribeWithInterface:
-            {
-                hub.Subscribe(new ErroringHandler());
-                sent = hub.Publish(new ErrorEvent());
-                Assert.False(sent);
-                Assert.Equal(4, errors);
-            }
-        }
+			Assert.True(sent, "did not send event to a known subscription");
+			Assert.Equal(1, handled);
+		}
 
-        [Fact]
-        public async void Multiple_subscriptions_with_different_results_should_be_pessimistic_and_sequential()
-        {
-            var hub = new Hub();
-            var pub = (IMessagePublisher) hub;
-            var sub = (IMessageAggregator) hub;
-            
-            // two handlers for the same event
-            sub.Subscribe(new ErroringHandler());           // false
-            sub.Subscribe(new NotErroringHandler());        // true
+		[Fact]
+		public async void Multiple_subscriptions_with_different_results_should_be_pessimistic_and_sequential()
+		{
+			var hub = new Hub();
+			var pub = (IMessagePublisher) hub;
+			var sub = (IMessageAggregator) hub;
 
-            // and one command to rule them all
-            var two = pub.PublishAsync(new ErrorEvent { Error = false });
-            var one = pub.PublishAsync(new ErrorEvent { Error = true });
+			// two handlers for the same event
+			sub.Subscribe(new ErroringHandler()); // false
+			sub.Subscribe(new NotErroringHandler()); // true
 
-            bool bad = await one;
-            Assert.False(bad, "whoops, outcomes are optimistic"); // <-- pessimistic
+			// and one command to rule them all
+			var two = pub.PublishAsync(new ErrorEvent {Error = false});
+			var one = pub.PublishAsync(new ErrorEvent {Error = true});
 
-            // now, send another message, because we have to ensure outcomes are cleared!
-            bool good = await two;
-            Assert.True(good, "whoops, outcomes are broken"); // <-- idempotent outcomes
-        }
-    }
+			var bad = await one;
+			Assert.False(bad, "whoops, outcomes are optimistic"); // <-- pessimistic
+
+			// now, send another message, because we have to ensure outcomes are cleared!
+			var good = await two;
+			Assert.True(good, "whoops, outcomes are broken"); // <-- idempotent outcomes
+		}
+	}
 }
--- src/reactive.pipes/Hub.Publish.cs
+++ src/reactive.pipes/Hub.Publish.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Daniel Crenna. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -10,79 +13,81 @@ namespace reactive.pipes
 		public DispatchConcurrencyMode DispatchConcurrencyMode = 0;
 
 		public Task<bool> PublishAsync(object message)
-        {
-            return Task.Run(() => Publish(message));
-        }
+		{
+			return Task.Run(() => Publish(message));
+		}
 
-        public bool Publish(object message)
-        {
-            var type = message.GetType();
-	        if (!(_byTypeDispatch[type] is Func<object, bool> dispatcher))
-            {
-                dispatcher = BuildByTypeDispatcher(type);
-                _byTypeDispatch[type] = dispatcher;
-            }
-            return dispatcher(message);
-        }
+		public bool Publish(object message)
+		{
+			var type = message.GetType();
+			if (!(_byTypeDispatch[type] is Func<object, bool> dispatcher))
+			{
+				dispatcher = BuildByTypeDispatcher(type);
+				_byTypeDispatch[type] = dispatcher;
+			}
 
-        private Func<object, bool> BuildByTypeDispatcher(Type superType)
-        {
-            var dispatchers = new Dictionary<Type, MethodInfo>
-            {
-                {superType, Constants.Methods.PublishTyped.MakeGenericMethod(superType)}
-            };
+			return dispatcher(message);
+		}
 
-            foreach (var childType in _typeResolver.GetAncestors(superType))
-                dispatchers.Add(childType, Constants.Methods.PublishTyped.MakeGenericMethod(childType));
+		private Func<object, bool> BuildByTypeDispatcher(Type superType)
+		{
+			var dispatchers = new Dictionary<Type, MethodInfo>
+			{
+				{superType, Constants.Methods.PublishTyped.MakeGenericMethod(superType)}
+			};
 
-	        bool Dispatch(object @event)
-	        {
-		        var result = true;
-		        foreach (var dispatcher in dispatchers)
-		        {
-			        var method = dispatcher.Value;
-			        var handled = (bool) method.Invoke(this, new[] {@event});
-			        result &= handled;
-		        }
-		        return result;
-	        }
+			foreach (var childType in _typeResolver.GetAncestors(superType))
+				dispatchers.Add(childType, Constants.Methods.PublishTyped.MakeGenericMethod(childType));
 
-	        return Dispatch;
-        }
-        
-        private bool PublishTyped<T>(T message)
-        {
-	        var key = SubscriptionKey.Create<T>(null);
+			bool Dispatch(object @event)
+			{
+				var result = true;
+				foreach (var dispatcher in dispatchers)
+				{
+					var method = dispatcher.Value;
+					var handled = (bool) method.Invoke(this, new[] {@event});
+					result &= handled;
+				}
 
-	        if (!_subscriptions.TryGetValue(key, out var subscription))
-		        return true;
+				return result;
+			}
 
-	        switch (DispatchConcurrencyMode)
-            {
-	            case DispatchConcurrencyMode.Default:
-		            lock (subscription)
+			return Dispatch;
+		}
+
+		private bool PublishTyped<T>(T message)
+		{
+			var key = SubscriptionKey.Create<T>(null);
+
+			if (!_subscriptions.TryGetValue(key, out var subscription))
+				return true;
+
+			switch (DispatchConcurrencyMode)
+			{
+				case DispatchConcurrencyMode.Default:
+					lock (subscription)
 						return ObserveOnSubject();
 				case DispatchConcurrencyMode.Unsafe:
-		            return ObserveOnSubject();
-	            default:
-		            throw new ArgumentOutOfRangeException();
-            }
-			
-            bool ObserveOnSubject()
-            {
-	            var subject = (WrappedSubject<T>) subscription;
-	            try
-	            {
-		            subject.Outcomes.Clear();
-		            subject.OnNext(message);
-		            return subject.Handled;
-	            }
-	            catch (Exception ex)
-	            {
-		            subject.OnError(ex); // <-- this kind of exception will cancel the observable sequence
-		            return false;
-	            }
-            }
-        }
-    }
+					return ObserveOnSubject();
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			bool ObserveOnSubject()
+			{
+				var subject = (WrappedSubject<T>) subscription;
+				try
+				{
+					subject.Outcomes.Clear();
+					subject.OnNext(message);
+					return subject.Handled;
+				}
+				catch (Exception ex)
+				{
+					subject.OnError(ex); // <-- this kind of exception will cancel the observable sequence
+					return false;
+				}
+			}
+		}
+	}
 }
